cd1fe6e1f3ab142331467cd080b315a8a4057fa0
hrengifo <hrengifo@bancolombia.com>
2018-08-19 03:52:15 -0500

    version 0.0.3 update pom.xml

--- WPIClient/src/main/java/com/calceteam/WPIClient/Application.java
+++ WPIClient/src/main/java/com/calceteam/WPIClient/Application.java
@@ -2,6 +2,8 @@ package com.calceteam.WPIClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -10,9 +12,14 @@ import com.calceteam.WPIClient.config.ApplicationConfig;
 @SpringBootApplication
 @Configuration
 @Import(ApplicationConfig.class)
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
 }
